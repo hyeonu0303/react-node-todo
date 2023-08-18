8a7d6a3266f2b81a2aad56561873ddcc4b0fe824
hyeonwoo <hyeonu030334@gmail.com>
2023-08-18 21:07:50 +0900

    로그인UI변경

--- webTodo-fronted/src/pages/login/login.jsx
+++ webTodo-fronted/src/pages/login/login.jsx
@@ -14,7 +14,36 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../store/userSlice";
 import googleLoginButton from "../../img/구글버튼/btn_google_signin_light_normal_web.png";
-import kakaoLoginButton from '../../img/카카오버튼/kakao_login_medium_narrow.png';
+import kakaoLoginButton from "../../img/카카오버튼/kakao_login_medium_narrow.png";
+import styled from "styled-components";
+
+/**컨테이너 */
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: var(--mainColor);
+`;
+const LoginBox = styled.div`
+  width: 75vh;
+  height: 75vh;
+  background-color: #ffffff;
+  padding:0 50px;
+  
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const LoginHeader = styled.div`
+  padding:50px;
+  
+`;
+
+const LoginContent = styled.div`
+  
+`
+
 function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
@@ -34,61 +63,71 @@ function Login() {
   };
   //user에서 res.json으로 보낸데이터값과 일치하면 보여주는식
   return (
-    <Container mt={5}>
-      <Center>
-        <Heading as="h2" size="lg">
-          로그인
-        </Heading>
-      </Center>
-      <Center>
-        <FormControl isRequired>
-          <FormLabel>아이디</FormLabel>
-          <Input type="text" value={email} onChange={handleSetId} />
-          <FormLabel>비밀번호</FormLabel>
-          <Input type="password" value={password} onChange={handleSetPw} />
-          <Button
-            colorScheme="linkedin"
-            variant="outline"
-            size="md"
-            mt={5}
-            onClick={() => {
-              axios
+    <LoginContainer>
+      <LoginBox>
+        <LoginHeader>
+          <Heading as='h2'>로그인</Heading>
+        </LoginHeader>
+        <LoginContent>
+          <FormControl isRequired>
+            <FormLabel>아이디</FormLabel>
+            <Input type="text" value={email} onChange={handleSetId} />
+            <FormLabel>비밀번호</FormLabel>
+            <Input type="password" value={password} onChange={handleSetPw} />
+            <Button
+              colorScheme="linkedin"
+              variant="outline"
+              style={{ width: "100%" }}
+              size="md"
+              mt={5}
+              onClick={() => {
+                axios
                 .post("/api/login", {
-                  email: email,
-                  password: password,
-                })
-                .then((result) => {
-                  const username = result.data.user.username;
-                  dispatch(login(username));
-                  navigate("/");
-                })
-                .catch((error) => {
-                  if (error.response) {
-                    console.error(
-                      "서버로부터의 응답:",
-                      error.response.data.message
-                    );
-                  } else {
-                    console.error("Axios 요청 오류:", error.message);
-                  }
-                });
-            }}
-          >
-            Login
-          </Button>
-          <br />
-          <Button
-            style={{padding:'0'}}
-            onClick={() => {
-              window.location.href = "/auth/google";
-            }}
-          >
-            <Image src={googleLoginButton}></Image>
-          </Button><br/><br/>
-          <Button style={{padding:'0'}} onClick={()=>{window.location.href='/auth/kakao'}}><Image src={kakaoLoginButton}></Image></Button>
-        </FormControl>
-      </Center>
-    </Container>
+                    email: email,
+                    password: password,
+                  })
+                  .then((result) => {
+                    const username = result.data.user.username;
+                    dispatch(login(username));
+                    navigate("/");
+                  })
+                  .catch((error) => {
+                    if (error.response) {
+                      console.error(
+                        "서버로부터의 응답:",
+                        error.response.data.message
+                      );
+                    } else {
+                      console.error("Axios 요청 오류:", error.message);
+                    }
+                  });
+                }}
+                >
+              Login
+            </Button>
+            <br />
+            <Button
+              style={{ padding: "0" }}
+              onClick={() => {
+                window.location.href = "/auth/google";
+              }}
+              >
+              <Image src={googleLoginButton}></Image>
+            </Button>
+            <br />
+            <br />
+            <Button
+              style={{ padding: "0" }}
+              onClick={() => {
+                window.location.href = "/auth/kakao";
+              }}
+            >
+              <Image src={kakaoLoginButton}></Image>
+            </Button>
+          </FormControl>
+        </LoginContent>
+      </LoginBox>
+    </LoginContainer>
   );
 }
 
