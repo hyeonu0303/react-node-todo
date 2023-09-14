8a3d3c8d2d84d6040ca56607809427b593e16d93
hyeonwoo <hyeonu030334@gmail.com>
2023-09-14 16:41:02 +0900

    fix: "register파일 소문자변경"

--- webTodo-fronted/src/router/router.jsx
+++ webTodo-fronted/src/router/router.jsx
@@ -1,7 +1,7 @@
 import App from '../App';
 // import ErrorPage from '../pages/ErrorPage';
 import Login from '@/pages/login/login';
-import Register from '@/pages/Register/Register';
+import Register from '@/pages/register/register';
 const RouterInfo = [
   {
     path: "/",
